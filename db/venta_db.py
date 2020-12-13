a816da397dc8d2b1cd9f91cf1b901a6c7e17136e
Antonia Bustamante <anattolia@gmail.com>
2020-12-13 13:25:16 -0500

    Update venta_db.py

--- db/venta_db.py
+++ db/venta_db.py
@@ -11,12 +11,29 @@ class VentaInDB(BaseModel):
     telefono: int
     username: str
 
+
+database_ventas = [{
+    "venta_id":1,
+    "venta_fecha":"2020-12-13T11:45:41.154306",
+    "venta_total":5000,
+    "telefono":3214567890,
+    "username": "Admin"
+    },
+    {"venta_id":2,
+    "venta_fecha":"2020-12-13T11:45:41.154306",
+    "venta_total":1000,
+    "telefono":3212222220,
+    "username": "Admin"
+    },
+]
+
 def get_all_ventas():
     return database_ventas
 
 
-database_ventas = []
-generator = {"id":0}
+#database_ventas = []
+len_lista = len(database_ventas)
+generator = {"id":len_lista}
 
 
 def save_venta(ventas_in_db: VentaInDB):
@@ -27,7 +44,7 @@ def save_venta(ventas_in_db: VentaInDB):
 
 
 def get_venta(id: int):
-    if ((id-1) < len(database_ventas)):
+    if (id != 0 and (id-1) < len(database_ventas)):
         return database_ventas[id-1]
     else:
         return "No existe la factura buscada"
