d217d73c60ce8593cc85dff044fbaf2ca9e81150
Antonia Bustamante <anattolia@gmail.com>
2020-12-13 10:29:00 -0500

    Update transaccion_db.py

--- db/transaccion_db.py
+++ db/transaccion_db.py
@@ -2,14 +2,14 @@ from typing import Dict
 from datetime import datetime
 from pydantic import BaseModel
 
-
 class TransaccionInDB(BaseModel):
     transaccion_id: int
-    producto_id: int
+    producto_id: str
     venta_id: int
     cant_pedido: int
-    tran_subtotal: int
+   # tran_subtotal: int
 
+"""
 database_transacciones = {
     "01": TransaccionInDB(**{"transaccion_id:":1,
                         "producto_id":1,
@@ -25,25 +25,31 @@ database_transacciones = {
                         "tran_subtotal": 10000
                         }),
 }
-
+"""
 
 def get_all_transacciones():
     return database_transacciones
 
 
 database_transacciones = []
-generator = {"id":0}
+generator_tr = {"id":0}
 
 
 def save_transaccion(transacciones_in_db: TransaccionInDB):
-    generator["id"] = generator["id"] + 1
-    transacciones_in_db.transaccion_id = generator["id"]
+    generator_tr["id"] = generator_tr["id"] + 1
+    transacciones_in_db.transaccion_id = generator_tr["id"]
     database_transacciones.append(transacciones_in_db)
     return transacciones_in_db
 
 
 def get_transaccion(id: int):
-    if id in database_transacciones:
-        return database_transacciones[id]
+    if ((id-1) < len(database_transacciones)):
+        return database_transacciones[id-1]
     else:
-        return None
+        return "No existe la transaccion buscada"
+
+"""
+def calcular_total(cant_pedido: int, precio: int):
+    total = cant_pedido * precio
+    return total
+    """
