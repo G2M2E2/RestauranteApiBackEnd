fe56f83d966602a12aadd09f3d70bc28acb1bb3d
Antonia Bustamante <anattolia@gmail.com>
2020-12-13 10:28:36 -0500

    Update transaccion_models.py

--- models/transaccion_models.py
+++ models/transaccion_models.py
@@ -1,14 +1,12 @@
 from pydantic import BaseModel
 
 class TransaccionIn(BaseModel):
-    producto_id: int
-    venta_id: int
     cant_pedido: int
-    tran_subtotal: int
+    producto_id: str
 
 class TransaccionOut(BaseModel):
     transaccion_id: int
-    producto_id: int
+    producto_id: str
     venta_id: int
     cant_pedido: int
-    tran_subtotal: int
+    #tran_subtotal: int
