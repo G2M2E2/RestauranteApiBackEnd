42adeda753ea17156016232087d1b2b2be5dad64
Antonia Bustamante <anattolia@gmail.com>
2020-12-13 20:40:50 -0500

    Update inventario_db.py

--- db/inventario_db.py
+++ db/inventario_db.py
@@ -24,6 +24,23 @@ database_productos = {
                                 }),
 }
 
+#Creada por Antonia para poder llamar la lista en la función crear_transaccion
+database_productos2 = [{
+    "id": "CA01",
+                                "nombre":"Churrasco",
+                                "precio":17000,
+                                "cantidad":10,
+                                "categoria":"Carnes"
+                                },
+
+    {"id": "PE01",
+                                "nombre":"Trucha frita",
+                                "precio":22000,
+                                "cantidad":15,
+                                "categoria":"Pescados"
+                                }
+]
+
 def create_producto(nuevo_producto: ProductoInDB):
     database_productos[nuevo_producto.id] = nuevo_producto
     return nuevo_producto
@@ -36,6 +53,10 @@ def get_producto(id: str):
 
 def get_all_productos():
     return database_productos.values()
+
+#Agregada por Antonia para poder llamarla en la función crear_transaccion
+def get_all_productos_dict():
+    return database_productos2
         
 def update_producto(producto_in_db: ProductoInDB):
     database_productos[producto_in_db.id] = producto_in_db
